b8fc65010d54327641e959665f5b8eaeb1d052ae
jack <biffisbuffm@gmail.com>
2020-08-30 15:33:33 -0500

    edit

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,47 +1,28 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-
 router.get('/', (req, res) => {
-    Comment.findAll({
-        attributes: [
-            'id',
-            'comment_text',
-            'user_id',
-            'post_id'
-        ],
-        
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 
 });
 
 router.post('/', (req, res) => {
-    // check the session
-    if (req.session) {
-      Comment.create({
+    Comment.create({
         comment_text: req.body.comment_text,
-        post_id: req.body.post_id,
-        // use the id from the session
-        user_id: req.session.user_id
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
       })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
           console.log(err);
           res.status(400).json(err);
         });
-    }
-  });
-
-router.delete('/', (req, res) => {
 
 });
 
-module.exports = router;
+router.delete('/:id', (req, res) => {
+
+});
+
 
 
 
