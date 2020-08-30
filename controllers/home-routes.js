c04f7913b2f15a46c04e124780c1006eb559775e
jack <biffisbuffm@gmail.com>
2020-08-30 17:34:42 -0500

    homne

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,17 +3,41 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_body: 'wig wig wig',
-        title: 'wig bank statement',
-        created_at: new Date(),
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
+ Post.findAll({
+     attributes: [
+         'id',
+         'post_body',
+         'title',
+         'created_at'
+     ],
+     include: [
+         {
+             model: Comment,
+             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+             include: {
+                 model: User,
+                 attributes: ['username']
+             }
+         },
+         {
+             model: User,
+             attributes: ['username']
+         }
+     ]
+ })
+ .then(dbPostData => {
+     console.log(dbPostData[0]);
 
-    });
+     const posts = dbPostData.map(post => post.get({ plain: true }));
+
+     res.render('homepage', { posts });
+ })
+ .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+ });
+
+
 });
 
 module.exports = router;
