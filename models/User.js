88f51ecd088c7809b594707f1c313ff6bead05c6
jack <biffisbuffm@gmail.com>
2020-08-30 13:27:27 -0500

    psw

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+const { beforeCreate, beforeUpdate } = require('../../just-tech-news/models/User');
 
 class User extends Model {}
 
@@ -33,6 +35,17 @@ User.init(
     }
 },
   {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
