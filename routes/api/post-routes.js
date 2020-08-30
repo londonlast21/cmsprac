fcfe7882ab992399c02a25c999ba5687ca60eab2
jack <biffisbuffm@gmail.com>
2020-08-30 14:47:36 -0500

    edits'

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,6 +5,7 @@ router.get('/', (req, res) => {
     console.log('===================');
     Post.findAll({
         attributes: ['id', 'post_body', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
@@ -48,5 +49,68 @@ router.get('/:id', (req, res) => {
 });
 
 
+router.post('/', (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.put('/:id', (req, res) => {
+    Post.update(
+    {
+        title: req.body.title
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    }
+)
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
+
 
 module.exports = router;
+
